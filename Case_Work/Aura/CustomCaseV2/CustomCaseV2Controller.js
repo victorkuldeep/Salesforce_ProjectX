c560ad4f555e76abff563b2f56f6e011459d6f08
Kuldeep Singh <victorkuldeep@gmail.com>
2024-06-30 14:11:16 +0530

    Edit Case Flexi Page Support Added along with View in Read Only

--- Case_Work/Aura/CustomCaseV2/CustomCaseV2Controller.js
+++ Case_Work/Aura/CustomCaseV2/CustomCaseV2Controller.js
@@ -1,63 +1,29 @@
 ({
     handleInit: function(component, event, helper) {
-        var overlayLib = component.find("overlayLib");
         
-        // Dynamically create the LWC component inside the modal
-        $A.createComponent(
-            
-            "c:newCaseLWC",
-            
-            { recordId: component.get("v.recordId") },
-            
-            function(newCaseLWC, status, errorMessage){
-            
-                if (status === "SUCCESS") {
-                
-                    var body = component.get("v.body") || [];
-                    body.push(newCaseLWC);
-                    component.set("v.body", body);
-                    
-                    overlayLib.showCustomModal({
-                        
-                        header: "New Case",
-                        body: newCaseLWC,
-                        showCloseButton: true,
-                        cssClass: "slds-modal_medium",
-                        
-                        closeCallback: function() {
-                        
-                            console.log('LWC Closed');
-                            
-                            // Callback function when modal is closed
-                            component.set("v.isModalOpen", false);
-                            
-                            // Navigate back to the Case tab list view
-                            var navService = component.find("navService");
-                            
-                            var pageReference = {
-                                type: 'standard__objectPage',
-                                attributes: {
-                                    objectApiName: 'Case',
-                                    actionName: 'list'
-                                },
-                                state: {}
-                            };
-                            
-                            // Navigate using the navigation service
-                            navService.navigate(pageReference).then(function(response) {
-                                console.log('Navigation Done');
-                            }).catch(function(error) {
-                                console.error('Navigation error: ' + JSON.stringify(error));
-                            });
-                        }
-                    });
-                    component.set("v.isModalOpen", true);
-                } else if (status === "INCOMPLETE") {
-                    console.log("No response from server or client is offline.");
-                } else if (status === "ERROR") {
-                    console.log("Error: " + errorMessage);
+        var overlayLib = component.find("overlayLib");
+        var recordId = component.get("v.recordId");
+        if(recordId){
+            var action = component.get("c.getCaseDetails");
+            action.setParams({
+                caseId: recordId
+            });
+            action.setCallback(this, function(response) {
+                var state = response.getState();
+                if (state === "SUCCESS") {
+                    var caseRecord = response.getReturnValue();
+                    var headerText = 'Edit Case - ' + caseRecord.CaseNumber + ' (' + caseRecord.Subject + ')';
+                    component.set("v.headerText", headerText);
+                    helper.createLWC(component, headerText);
+                } else {
+                    console.error("Failed to fetch Case details with state: " + state);
                 }
-            }
-        );
+            });
+            $A.enqueueAction(action);
+        }else{
+            var headerText = 'New Case';
+            component.set("v.headerText", headerText);
+            helper.createLWC(component, headerText);
+        }
     }
 })
