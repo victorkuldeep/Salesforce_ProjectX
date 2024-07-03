dd09a056ad0ea29794885071983c0511c6331652
Kuldeep Singh <victorkuldeep@gmail.com>
2024-07-04 01:25:33 +0530

    Dynamic On Change Support added and refactored contact creation with defined record type

--- Case_Work/Lightning Web Component/newCaseLWC/fieldMapper.js
+++ Case_Work/Lightning Web Component/newCaseLWC/fieldMapper.js
@@ -1,18 +1,87 @@
 // recordTypeFieldMap.js
-const leftColumnFields = [
-    { fieldName: 'Subject', required: true },
-    { fieldName: 'Origin', required: true },
-    { fieldName: 'Priority', required: false },
-    { fieldName: 'Reason', required: true },
-    { fieldName: 'OwnerId', required: false, outputField: true }
+const fieldMapperContact = {
+    default: [
+        { fieldName: 'FirstName', required: true, disabled: false },
+        { fieldName: 'LastName', required: true, disabled: false },
+        { fieldName: 'Email', required: false, disabled: false },
+        { fieldName: 'Phone', required: false, disabled: false },
+        { fieldName: 'Title', required: false, disabled: false },
+        { fieldName: 'Department', required: false, disabled: false }
+    ]
+};
+
+const lastSection = 'Account And Contact Details'
+const fieldConfig = [
+    {
+        sectionName: 'Case Details',
+        columns: [
+            [
+                { apiName: 'Subject', required: true },
+                { apiName: 'Status', required: true },
+                { apiName: 'Origin', required: false }
+                
+            ],
+            [
+                { apiName: 'Priority', required: true }
+            ]
+        ]
+    },
+    {
+        sectionName: 'Address Details',
+        columns: [
+            [
+                { apiName: 'EngineeringReqNumber__c', required: false },
+                { apiName: 'IsEscalated', required: false }
+            ],
+            [
+                { apiName: 'Comments', required: false }
+                
+            ]
+        ]
+    }
 ];
 
-const rightColumnFields = [
-    { fieldName: 'ParentId', required: false },
-    { fieldName: 'Status', required: true },
-    { fieldName: 'Description', required: false },
-    { fieldName: 'AccountId', required: true, hasOnchange: true },
-    { isCombobox: true, label: 'Contact', hasOnchange: true }
-];
+const valueChangeMapper = {
+    Status: [
+        { 
+            New: [
+                { apiName: 'EngineeringReqNumber__c', required: true }, 
+                { apiName: 'Target_Date__c', required: false },
+                { apiName: 'SuppliedPhone', required: true },
+                { apiName: 'SuppliedName', required: false }
+            ],
+            Working: [
+                { apiName: 'SuppliedPhone', required: true },
+                { apiName: 'SuppliedName', required: false }
+            ],
+            Closed: [
+                { apiName: 'SuppliedName', required: true },
+                { apiName: 'Target_Date__c', required: false }
+            ],
+            Escalated: [
+                { apiName: 'Target_Date__c', required: true },
+                { apiName: 'SuppliedPhone', required: false }
+            ]
+        }
+    ],
+    Origin: [
+        { 
+            Phone: [
+                { apiName: 'Target_Date__c', required: true }, 
+                { apiName: 'SuppliedEmail', required: false }
+            ],
+            Email: [
+                { apiName: 'SuppliedEmail', required: true },
+                { apiName: 'SuppliedPhone', required: false }
+            ],
+            Web: [
+                { apiName: 'SuppliedPhone', required: true },
+                { apiName: 'EngineeringReqNumber__c', required: false }
+            ]
+        }
+    ]
+};
 
-export {leftColumnFields,rightColumnFields};
+
+
+export {fieldMapperContact,fieldConfig,valueChangeMapper,lastSection};
